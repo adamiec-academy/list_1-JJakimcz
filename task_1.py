c583bf2703cfc34630604f738376634d6e908d9f
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-10-11 19:13:48 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,20 @@
+def linia_gd(n):
+    for _ in range(n):
+        print(" ", end = '')
+    for _ in range(n):
+        print("*", end = '')
+    print()
+    
+def linia_sr(n):
+    for _ in range(n*3):
+        print("*", end = '')
+    print()
+
 def cross(n):
-    pass
+    for _ in range(n):
+        linia_gd(n)
+    for _ in range(n):
+        linia_sr(n)
+    for _ in range(n):
+        linia_gd(n)
+    
