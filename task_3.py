879b416774a46e022b4eecc017efce3a42f1a531
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-10-19 18:35:58 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,20 @@
+n = int(input("podaj n: "))
 def envelope(n):
-    pass
+    z=0
+    a=0
+    b=n+1
+    print((2*n+1)*"*", end = '')
+    print()
+    for _ in range(n-1):
+        print("*" + a * " " + "*" + b * " " + "*" + a * " " + "*")
+        a=a+1
+        b=b-2
+    print("*" + (n-1) * " " + "*" + (n-1) * " " + "*") 
+    for _ in range(n-1):
+        a=a-1
+        b=b+2
+        print("*" + a * " " + "*" + b * " " + "*" + a * " " + "*")   
+ 
+    print((2*n+1)*"*", end = '')
+
+envelope(n)
