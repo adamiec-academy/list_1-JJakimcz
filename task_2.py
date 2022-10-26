2e3fe3242a83556922a671d2ec62757a6cf3e174
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-10-26 10:56:46 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,31 +1,12 @@
-def kwadratszach(n,k):
-    for _ in range(k):
-        print("#", end = '')
-
-def kwadratpusty(n,k):
-    for _ in range(k):
-        print(" ", end = '')
-
-def l1(n,k):
-    for _ in range(k):          
-        print()
-        for _ in range(n):
-            kwadratszach(n,k)
-            kwadratpusty(n,k)
-
-def l2(n,k):
-    for _ in range(k):          
-        print()
-        for _ in range(n):
-            kwadratpusty(n,k)
-            kwadratszach(n,k)
-
 def chess_board(n,k):
-    i=0
+
     for _ in range(n):
-        i=i+1
-        if i==n+1:
-            break
-        else:
-            l2(n,k)
-            l1(n,k)
+        for _ in range(k):
+            for _ in range(n):
+                print((" " * k) + ("#" * k), end="")
+            print()
+
+        for _ in range(k):
+            for _ in range(n):    
+                print(("#" * k) + (" " * k), end="")
+            print()
