3c9bc37fe1f1a17c209150f61609ff0c42f05c5d
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-10-26 11:35:49 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,17 +1,14 @@
 def envelope(n):
-    z=0
     a=0
-    b=n+1
-    print((2*n+1)*"*", end = '')
-    print()
-    for _ in range(n-1):
-        print("*" + a * " " + "*" + b * " " + "*" + a * " " + "*")
+    b=(2*n-3)
+    print("*" * (2*n+1))
+    for _ in range((n-1)):
+        print("*"+ " " * a + "*" + " " * b + "*" + " " * a + "*")
         a=a+1
         b=b-2
-    print("*" + (n-1) * " " + "*" + (n-1) * " " + "*") 
-    for _ in range(n-1):
+    print("*"+ " " * a + "*" + " " * a +"*")
+    for _ in range((n-1)):
         a=a-1
         b=b+2
-        print("*" + a * " " + "*" + b * " " + "*" + a * " " + "*")   
- 
-    print((2*n+1)*"*", end = '')
+        print("*"+ " " * a + "*" + " " * b + "*" + " " * a + "*")
+    print("*" * (2*n+1))
